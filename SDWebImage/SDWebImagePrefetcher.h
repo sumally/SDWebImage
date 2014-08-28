4eb6fcdc829ec60bfc1e71deb8d377d947c2344a
Takeru Chuganji <takeru@hoppenichu.com>
2014-08-28 20:30:38 +0900

    API追加

--- SDWebImage/SDWebImagePrefetcher.h
+++ SDWebImage/SDWebImagePrefetcher.h
@@ -12,59 +12,68 @@
 typedef void(^SDWebImagePrefetcherProgressBlock)(NSURL *url);
 
 /**
- * Prefetch some URLs in the cache for future use. Images are downloaded in low priority.
- */
+* Prefetch some URLs in the cache for future use. Images are downloaded in low priority.
+*/
 @interface SDWebImagePrefetcher : NSObject
 
 /**
- *  The web image manager
- */
+*  The web image manager
+*/
 @property (strong, nonatomic, readonly) SDWebImageManager *manager;
 
 /**
- * Maximum number of URLs to prefetch at the same time. Defaults to 3.
- */
+* Maximum number of URLs to prefetch at the same time. Defaults to 3.
+*/
 @property (nonatomic, assign) NSUInteger maxConcurrentDownloads;
 
 /**
- * SDWebImageOptions for prefetcher. Defaults to SDWebImageLowPriority.
- */
+* SDWebImageOptions for prefetcher. Defaults to SDWebImageLowPriority.
+*/
 @property (nonatomic, assign) SDWebImageOptions options;
 
 /**
- * Return the global image prefetcher instance.
- */
+* Return the global image prefetcher instance.
+*/
 + (SDWebImagePrefetcher *)sharedImagePrefetcher;
 
 /**
- * Assign list of URLs to let SDWebImagePrefetcher to queue the prefetching,
- * currently one image is downloaded at a time,
- * and skips images for failed downloads and proceed to the next image in the list
- *
- * @param urls list of URLs to prefetch
- */
+* Assign list of URLs to let SDWebImagePrefetcher to queue the prefetching,
+* currently one image is downloaded at a time,
+* and skips images for failed downloads and proceed to the next image in the list
+*
+* @param urls list of URLs to prefetch
+*/
 - (void)prefetchURLs:(NSArray *)urls;
 
 /**
- * Assign list of URLs to let SDWebImagePrefetcher to queue the prefetching,
- * currently one image is downloaded at a time,
- * and skips images for failed downloads and proceed to the next image in the list
- *
- * @param urls            list of URLs to prefetch
- * @param progressBlock   block to be called when progress updates; 
- *                        first parameter is the number of completed (successful or not) requests, 
- *                        second parameter is the total number of images originally requested to be prefetched
- * @param completionBlock block to be called when prefetching is completed
- *                        first param is the number of completed (successful or not) requests,
- *                        second parameter is the number of skipped requests
- */
+* Assign list of URLs to let SDWebImagePrefetcher to queue the prefetching,
+* currently one image is downloaded at a time,
+* and skips images for failed downloads and proceed to the next image in the list
+*
+* @param urls            list of URLs to prefetch
+* @param progressBlock   block to be called when progress updates;
+*                        first parameter is the number of completed (successful or not) requests,
+*                        second parameter is the total number of images originally requested to be prefetched
+* @param completionBlock block to be called when prefetching is completed
+*                        first param is the number of completed (successful or not) requests,
+*                        second parameter is the number of skipped requests
+*/
 - (void)prefetchURLs:(NSArray *)urls progress:(SDWebImagePrefetcherProgressBlock)progressBlock completed:(SDWebImageNoParamsBlock)completionBlock;
+
+
+- (void)removeURLFromStack:(NSURL *)url cancel:(BOOL)cancel;
 
 - (void)cancelPrefetchingForURL:(NSURL *)url;
 /**
- * Remove and cancel queued list
- */
+* Remove and cancel queued list
+*/
 - (void)cancelPrefetching;
 
+- (BOOL)isCurrentProceeding:(NSURL *)url;
+
+- (BOOL)containsOnStack:(NSURL *)url;
+
+- (BOOL)waitComplete:(NSURL *)url handler:(SDWebImageCompletionBlock)handler;
+- (void)cancelWaitingComplete:(NSURL *)url;
 
 @end
--- SDWebImage/SDWebImagePrefetcher.m
+++ SDWebImage/SDWebImagePrefetcher.m
@@ -12,8 +12,10 @@
 
 @property (strong, nonatomic) SDWebImageManager *manager;
 @property (strong, nonatomic) NSMutableArray *prefetchURLs;
+@property (strong, nonatomic) NSMutableArray *proceedingURLs;
 @property (copy, nonatomic) SDWebImageNoParamsBlock completionBlock;
 @property (copy, nonatomic) SDWebImagePrefetcherProgressBlock progressBlock;
+@property (strong, nonatomic) NSMutableDictionary *handlers;
 
 @end
 
@@ -33,6 +35,8 @@
         _manager = [SDWebImageManager new];
         _options = SDWebImageLowPriority;
         _prefetchURLs = [NSMutableArray array];
+        _proceedingURLs = [NSMutableArray array];
+        _handlers = [NSMutableDictionary dictionary];
         self.maxConcurrentDownloads = 3;
     }
     return self;
@@ -52,9 +56,16 @@
     }
     id url = self.prefetchURLs[0];
     [self.prefetchURLs removeObjectAtIndex:0];
+    [self.proceedingURLs addObject:url];
     [self.manager downloadImageWithURL:url options:self.options progress:nil completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
         if (!finished) {
             return;
+        }
+        [self.proceedingURLs removeObject:url];
+        SDWebImageCompletionBlock handler = self.handlers[imageURL];
+        if (handler) {
+            handler(image, error, cacheType, imageURL);
+            [self.handlers removeObjectForKey:imageURL];
         }
 
         if (self.progressBlock) {
@@ -82,9 +93,22 @@
     [self.prefetchURLs addObjectsFromArray:urls];
     self.completionBlock = completionBlock;
     self.progressBlock = progressBlock;
-    for (NSUInteger i = 0; i < self.maxConcurrentDownloads; i++) {
+    for (NSUInteger i = [self.proceedingURLs count]; i < self.maxConcurrentDownloads; i++) {
         [self startPrefetching];
     }
+}
+
+- (void)removeURLFromStack:(NSURL *)url cancel:(BOOL)cancel {
+    if (!url) {
+        return;
+    }
+    BOOL isCurrent = [self isCurrentProceeding:url];
+    if (isCurrent && cancel) {
+        [self cancelPrefetchingForURL:url];
+    } else {
+        [self.prefetchURLs removeObject:url];
+    }
+    [self.handlers removeObjectForKey:url];
 }
 
 - (void)cancelPrefetchingForURL:(NSURL *)url {
@@ -92,11 +116,54 @@
     // in case the array is const of NSString
     [self.prefetchURLs removeObject:url.absoluteString];
     [self.manager cancelForURL:url];
+
+    [self.proceedingURLs removeObject:url];
+    // in case the array is const of NSString
+    [self.proceedingURLs removeObject:url.absoluteString];
+
+    [self.handlers removeObjectForKey:url];
+
+    for (NSUInteger i = [self.proceedingURLs count]; i < self.maxConcurrentDownloads; i++) {
+        [self startPrefetching];
+    }
 }
 
 - (void)cancelPrefetching {
     [self.prefetchURLs removeAllObjects];
     [self.manager cancelAll];
+    [self.proceedingURLs removeAllObjects];
+    [self.handlers removeAllObjects];
+}
+
+- (BOOL)isCurrentProceeding:(NSURL *)url {
+    BOOL isExist = [self.proceedingURLs containsObject:url];
+    if (!isExist) {
+        isExist = [self.proceedingURLs containsObject:url.absoluteString];
+    }
+    return isExist;
+}
+
+- (BOOL)containsOnStack:(NSURL *)url {
+    BOOL contains = [self.prefetchURLs containsObject:url];
+    if (!contains) {
+        contains = [self.prefetchURLs containsObject:url.absoluteString];
+    }
+    return contains;
+}
+
+- (BOOL)waitComplete:(NSURL *)url handler:(SDWebImageCompletionBlock)handler {
+    if ([self containsOnStack:url]) {
+        return NO;
+    }
+    self.handlers[url] = [handler copy];
+    return YES;
+}
+
+- (void)cancelWaitingComplete:(NSURL *)url {
+    if (!url) {
+        return;
+    }
+    [self.handlers removeObjectForKey:url];
 }
 
 @end
